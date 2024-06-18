fab91dde9cbf05f181bb331ec066553fb27d8832
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-06-18 16:21:31 +0100

    Add band_name project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# flutter
-Playground for projects as I learn Flutter 
+# Flutter
+
+This is a playground for projects as I learn Flutter. Nothing here is intended to be polished or even functional.
+
+Tutorials followed so far:
+
+1. [hello_world](/hello_world/) – A ‘Hello, world’ button click counter adapted from Flutter’s [‘Test drive’](https://docs.flutter.dev/get-started/test-drive) getting started project.
+2. [band_name](/band_name/) – A band name generator adapted from Flutter’s [‘Write your first app’](https://docs.flutter.dev/get-started/codelab) codelab.
---
+++ band_name/lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Strings {
+  static const appTitle = 'Band name';
+  static const buttonLabel = 'Sick! Give me another';
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: Strings.appTitle,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var wordz = WordPair.random();
+
+  void getNext() {
+    wordz = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var buttonLabel = "${appState.wordz.asPascalCase} 🤘";
+    var semanticsLabel = "${appState.wordz.first} ${appState.wordz.second}";
+
+    var theme = Theme.of(context);
+    var buttonStyle = ElevatedButton.styleFrom(
+        backgroundColor: theme.colorScheme.background,
+        foregroundColor: theme.colorScheme.onBackground);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: BigCard(buttonLabel, semanticsLabel: semanticsLabel),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: buttonStyle,
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text(Strings.buttonLabel),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard(
+    this.data, {
+    this.semanticsLabel,
+    super.key,
+  });
+
+  final String data;
+  final String? semanticsLabel;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.background,
+    );
+
+    return Card(
+      color: theme.colorScheme.onBackground,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(data, semanticsLabel: semanticsLabel, style: style),
+      ),
+    );
+  }
+}
