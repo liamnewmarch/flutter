9d68a738b12d5fa1d805f8a11c9e8eaea52ce594
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-06-20 09:24:11 +0100

    Run `flutter create .` to add all OS targets. Update dependencies

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
