9155439955ec6e13a2eb823a5e7e511d038e0766
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-06-26 11:34:55 +0100

    Fix linter warnings, add generated files

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const TodoApp());
 
-class MyApp extends StatelessWidget {
+class TodoApp extends StatelessWidget {
+  const TodoApp({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<Data>(
-      builder: (context) => Data(),
+      create: (context) => Data(),
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: MyText(),
+            title: const MyText(),
           ),
-          body: Level1(),
+          body: const Level1(),
         ),
       ),
     );
@@ -21,18 +25,24 @@ class MyApp extends StatelessWidget {
 }
 
 class Level1 extends StatelessWidget {
+  const Level1({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level2(),
-    );
+    return const Level2();
   }
 }
 
 class Level2 extends StatelessWidget {
+  const Level2({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
         MyTextField(),
         Level3(),
@@ -42,6 +52,8 @@ class Level2 extends StatelessWidget {
 }
 
 class Level3 extends StatelessWidget {
+  const Level3({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Text(Provider.of<Data>(context).data);
@@ -49,6 +61,10 @@ class Level3 extends StatelessWidget {
 }
 
 class MyText extends StatelessWidget {
+  const MyText({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Text(Provider.of<Data>(context, listen: false).data);
@@ -56,6 +72,10 @@ class MyText extends StatelessWidget {
 }
 
 class MyTextField extends StatelessWidget {
+  const MyTextField({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
     return TextField(
