f9b564d12b2e5e5f941f5212a1ac3eda566959a8
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-07-15 10:37:18 +0100

    Update Android targets for bmi calculator and xylophone apps

--- xylophone/ios/Flutter/flutter_export_environment.sh
+++ xylophone/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.19.5/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/liam/code/temp/flutter/xylophone"
+export "FLUTTER_APPLICATION_PATH=/Users/liam/code/temp/flutter/learn-flutter/xylophone"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
